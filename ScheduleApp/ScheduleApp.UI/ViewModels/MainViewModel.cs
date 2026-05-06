af68c4a6fb9c05eea591a95e3b6c6e28fccc4ee3
Heily Rios <heilyy.riosa@autonoma.edu.co>
2026-05-05 23:33:22 -0500

    feat(#43): agregar estructura, vistas y viewmodels para el menú lateral

---
+++ ScheduleApp/ScheduleApp.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ScheduleApp.UI.Services;
+using ScheduleApp.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ScheduleApp.UI.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowDashboardCommand { get; set; }
+        public ICommand ShowMateriasCommand { get; set; }
+
+        public MainViewModel()
+        {
+            ShowDashboardCommand =
+                new RelayCommand(o =>
+                {
+                    CurrentView = new DashboardView();
+                });
+
+            ShowMateriasCommand =
+                new RelayCommand(o =>
+                {
+                    CurrentView = new MateriasView();
+                });
+
+            CurrentView = new DashboardView();
+        }
+    }
+}
