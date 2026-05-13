97c172139096963f8127bf055c72a35ed5017a1d
Heily Rios <heilyy.riosa@autonoma.edu.co>
2026-05-13 18:22:42 -0500

    feat: agregar UI, ViewModel y modelo para la lista de materias

--- ScheduleApp/ScheduleApp.UI/ViewModels/MateriasViewModel.cs
+++ ScheduleApp/ScheduleApp.UI/ViewModels/MateriasViewModel.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ScheduleApp.UI.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace ScheduleApp.UI.ViewModels
 {
-    internal class MateriasViewModel
+    public class MateriasViewModel
     {
+        public ObservableCollection<SubjectModel> Subjects { get; set; }
+
+        private readonly HttpClient _httpClient;
+
+        public MateriasViewModel()
+        {
+            Subjects = new ObservableCollection<SubjectModel>();
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7216/")
+            };
+
+            _ = LoadSubjects();
+        }
+
+        private async Task LoadSubjects()
+        {
+            try
+            {
+                var subjects = await _httpClient
+                    .GetFromJsonAsync<ObservableCollection<SubjectModel>>("api/subjects");
+
+                if (subjects != null)
+                {
+                    Subjects.Clear();
+
+                    foreach (var subject in subjects)
+                    {
+                        Subjects.Add(subject);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
